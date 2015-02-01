45b086efbeef2dd2ebc0da6085c2e236224c1c76
Niklas Mollenhauer <nikeee@outlook.com>
2015-02-01 07:17:28 +0100

    Fix OS detection for non-windows systems

--- src/NTH.Windows.Forms/NTH.Windows.Forms/EnvironmentEx.cs
+++ src/NTH.Windows.Forms/NTH.Windows.Forms/EnvironmentEx.cs
@@ -10,7 +10,18 @@ namespace NTH.Windows.Forms
     {
         public static bool IsDwmSupported
         {
-            get { return Environment.OSVersion.Version >= OsVersions.Vista; }
+            get
+            {
+                // If running on Linux or Mac or XBox, there will be no (Windows-)DWM.
+                if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+                    return false;
+                return Environment.OSVersion.Version >= OsVersions.Vista;
+            }
+        }
+
+        internal static bool IsWindowsEightOrHigher
+        {
+            get { return Environment.OSVersion.Version >= OsVersions.Eight; }
         }
 
         public static bool IsDwmEnabled
@@ -19,6 +30,14 @@ namespace NTH.Windows.Forms
             {
                 if (!IsDwmSupported)
                     return false;
+
+                // Note: As of Windows 8, DWM composition is always enabled.
+                // Source: https://msdn.microsoft.com/en-us/library/windows/desktop/aa969518%28v=vs.85%29.aspx
+                if (IsWindowsEightOrHigher)
+                    return true;
+
+                // So the API call is only necessary when using Windows 7 or Windows Vista.
+
                 bool isEnabled;
                 int result;
                 if ((result = NativeMethods.DwmIsCompositionEnabled(out isEnabled)) != 0)
