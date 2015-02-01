8c7ee1f1f91f65c93c71a5921f976a0debf5450e
Niklas Mollenhauer <nikeee@outlook.com>
2015-02-01 07:42:19 +0100

    Move credentials to seperate files

--- src/NTH.Windows.Forms/NTH.Windows.Forms/CredentialDialog.cs
+++ src/NTH.Windows.Forms/NTH.Windows.Forms/CredentialDialog.cs
@@ -49,22 +49,4 @@ namespace NTH.Windows.Forms
             return null;
         }
     }
-
-    public class Credentials
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-
-        public Credentials()
-            : this(null)
-        { }
-        public Credentials(string username)
-            : this(username, null)
-        { }
-        public Credentials(string username, string password)
-        {
-            Username = username;
-            Password = password;
-        }
-    }
 }
---
+++ src/NTH.Windows.Forms/NTH.Windows.Forms/Credentials.cs
@@ -0,0 +1,20 @@
+﻿namespace NTH.Windows.Forms
+{
+    public class Credentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Credentials()
+            : this(null)
+        { }
+        public Credentials(string username)
+            : this(username, null)
+        { }
+        public Credentials(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
